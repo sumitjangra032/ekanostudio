8037534db869009745b06a96a3ebbdb58b0622d6
sumitjangra032 <sumitjangra032@gmail.com>
2026-02-26 20:40:08 +0530

    gooole warning of watch pages has been fixed

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,55 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  async headers() {
+    return [
+      {
+        source: "/portfolio-new/assets/:path*.mp4",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+      {
+        source: "/portfolio-new/assets/:path*.webm",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+      {
+        source: "/portfolio-new/assets/:path*.mov",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+      {
+        source: "/portfolio-new/assets/:path*.m4v",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+      {
+        source: "/portfolio-new/assets/:path*.avi",
+        headers: [
+          {
+            key: "X-Robots-Tag",
+            value: "noindex",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
