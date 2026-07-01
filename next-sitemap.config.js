0c0427f0afe50d890bf03f16b41e6a23078ac77c
opchimnani <omprakash.chimnani@w3eera.com>
2026-07-01 14:48:28 +0530

    Added the Excluded urls for sitemaps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,11 @@ module.exports = {
     siteUrl: "https://www.ekanostudio.com",
     generateRobotsTxt: true,
     sitemapSize: 7000,
+    exclude: [
+        "/website-design-hisar",
+        "/seo-services-hisar",
+        "/content-creation-hisar",
+        "/digital-marketing-hisar",
+        "/ai-automation-hisar",
+    ],
 };
